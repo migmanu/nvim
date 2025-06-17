eb69505a789d85f4dd53bb899b7f0485ca07eeec
migmanu <jmanuelmigoya@gmail.com>
2025-06-17 21:18:55 +0200

    feat: add refactor

--- init.lua
+++ init.lua
@@ -276,6 +276,17 @@ require('lazy').setup({
   -- why not?
   'eandrju/cellular-automaton.nvim',
 
+  -- refactor
+  {
+    'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    lazy = false,
+    opts = {},
+  },
+
   -- leap
   { 'ggandor/leap.nvim' },
 
